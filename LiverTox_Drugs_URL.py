b25a3fa959c3b40aed07035da515a2864f154040
jieqiangzhu <zhujieqiang@gmail.com>
2018-04-08 23:20:07 -0500

    Create LiverTox_Drugs_URL.py

---
+++ LiverTox_Drugs_URL.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  5 09:02:08 2018
+
+@author: Jieqiang.Zhu
+"""
+import requests
+import string
+import time
+import csv
+from lxml import etree
+
+def get_drug_list(url):
+    content = requests.get(url).content
+    html = etree.HTML(content)
+    result_name = html.xpath('//h3/ol/li/a/text()')
+    result_url = html.xpath('//h3/ol/li/a/@href')
+    result = dict(zip(result_name,result_url))
+    return result
+
+urls = []
+for alphabet in string.ascii_uppercase:
+	urls.append('https://livertox.nih.gov/php/searchchem.php?chemrang='+alphabet)
+
+drug_pages = {}
+
+for each in urls:
+    drug_pages.update(get_drug_list(each))
+    time.sleep(20)
+
+with open ('drug_pages.csv','w') as f:
+    w = csv.DictWriter(f, drug_pages.keys())
+    w.writeheader()
+    w.writerow(drug_pages)
